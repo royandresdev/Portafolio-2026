907e17f08b89ba152c05dbd2f0a53f99a022aad3
royandresdev <royhuamanavila@gmail.com>
2026-04-18 07:51:33 -0500

    feat: add responsive header component with navigation and logo

--- components/header.tsx
+++ components/header.tsx
@@ -1,13 +1,14 @@
+import { Icon } from "@iconify/react";
 import Image from "next/image";
 
 export function Header() {
   return (
     <div className="w-full bg-[#0000001a] backdrop-blur-[2px] fixed top-0 z-50">
-      <header className="relative flex justify-center items-center h-18 container mx-auto px-6 max-w-[1280px]">
+      <header className="relative flex justify-end lg:justify-center items-center h-18 container mx-auto px-6 max-w-[1280px]">
         <div className="absolute left-6">
-          <Image src="/logo.svg" alt="Logo" height={48} width={43} loading="eager" />
+          <Image className="h-8 w-auto lg:h-12" src="/logo.svg" alt="Logo" height={48} width={43} loading="eager" />
         </div>
-        <nav>
+        <nav className="hidden lg:block">
           <ul className="flex gap-3 text-gray-5 font-bold">
             <li>
               <a href="#" className="text-primary">ACERCA DE MI</a>
@@ -23,6 +24,9 @@ export function Header() {
             </li>
           </ul>
         </nav>
+        <button className="lg:hidden">
+          <Icon className="size-8 text-primary" icon="material-symbols:menu-rounded" />
+        </button>
       </header>
     </div>
   )
