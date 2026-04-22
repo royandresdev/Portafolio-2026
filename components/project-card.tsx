68f96d5c111cf74700b7b08c1c77a4669d4fb9d8
royandresdev <royhuamanavila@gmail.com>
2026-04-22 10:17:00 -0500

    feat: add ProjectCard component to display project details and links

--- components/project-card.tsx
+++ components/project-card.tsx
@@ -11,9 +11,10 @@ interface ProjectCardProps {
   linkDemo: string;
   linkRepo: string;
   linkFigma?: string;
+  typeApp: string;
 }
 
-export function ProjectCard({ name, description, image, technologies, linkDemo, linkRepo }: ProjectCardProps) {
+export function ProjectCard({ name, description, image, technologies, linkDemo, linkRepo, typeApp }: ProjectCardProps) {
   return (
     <article className="w-full h-full bg-black-3 rounded-[32px] pb-6 custom-shadow flex flex-col border border-black-3">
       <div className="w-full aspect-video relative overflow-hidden rounded-t-[32px]">
@@ -21,7 +22,7 @@ export function ProjectCard({ name, description, image, technologies, linkDemo, 
       </div>
       <div className="p-6 space-y-4">
         <h3 className="text-gray-5 uppercase font-bold text-xl lg:text-2xl">{name}</h3>
-        <p className="text-primary font-semibold">Pagina Industrial</p>
+        <p className="text-primary font-semibold">{typeApp}</p>
         <Tags technologies={technologies} />
         <p className="text-gray-4">{description}</p>
       </div>
