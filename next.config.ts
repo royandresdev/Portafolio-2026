4fa9d93ef616f79416dc4ad1f01555d2aa1a2b05
royandresdev <royhuamanavila@gmail.com>
2026-04-23 15:24:48 -0500

    feat: add Supabase storage hostname to image remote patterns in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,12 @@ const nextConfig: NextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "vggtrffoascdbyqxdccw.supabase.co",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
